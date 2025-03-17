f481b512ce064e317789abe54685a4b0622b74fd
Dominic Johnson <dominicjohnson83@gmail.com>
2025-03-18 06:19:10 +1300

    Switch to standard Next.js deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  trailingSlash: true,
-  distDir: 'out',
-  assetPrefix: '.'
+  reactStrictMode: true
 }
 
 module.exports = nextConfig 
